b969629604f4b17496b8acc969b6bd069b86af7f
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2021-12-25 09:40:09 -0700

    have dict write to text file and ave grades

--- import_dictionary.py
+++ import_dictionary.py
@@ -1,9 +1,17 @@
 import csv
-
-grades_dict = {}
+sum = 0
+n = 0
 
 with open("grades.csv", "r") as file:
     reader = csv.reader(file)
     grades_dict = {rows[0]: rows[1] for rows in reader}
 
-print(grades_dict)
+with open("grades.txt", "a", encoding="utf-8") as text_file:
+    for key, value in grades_dict.items():
+        text_file.write(key + ": " + value)
+        text_file.write("\n")
+        if value != "Grade":
+            sum += int(value)
+        n += 1
+    average = sum / n
+    text_file.write("Class average: " + str(round(average, 2)))
