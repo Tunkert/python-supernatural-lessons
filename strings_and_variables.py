4085bff04801ce2a2fa2ef11166067184b9673bc
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2021-12-28 21:42:15 -0700

    print strings, concat, variables with sup char

---
+++ strings_and_variables.py
@@ -0,0 +1,45 @@
+# Supernatural lesson - strings with variables
+
+# We'll discuss printing out strings, Concatenation, and f strings.
+
+print("Castiel is an angel on Supernatural.")
+print("Dean is a human on Supernatural")
+print("Crowley is a demon on Supernatural")
+print("Gabriel is an archangel on Supernatural")
+
+# Let's say we want to change the names.
+# We can rewrite this with variables
+
+# Variables
+angel = "Castiel"
+human = "Dean"
+demon = "Crowley"
+archangel = "Gabriel"
+
+
+# Concatenation method - pre 3.6
+print(angel + " is an angel on Supernatural.")
+print(human + " is an human on Supernatural.")
+print(demon + " is an demon on Supernatural.")
+print(archangel + " is an archangel on Supernatural.")
+
+
+# 3.6 and higher
+print(f"{angel} is an angel on Supernatural.")
+print(f"{human} is a human on Supernatural.")
+print(f"{demon} is a demon on Supernatural.")
+print(f"{archangel} is an archangel on Supernatural.")
+
+
+# Variables allow you to change character names.
+angel = "Anna"
+human = "Sam"
+demon = "Lilith"
+archangel = "Raphael"
+
+
+# print with new names
+print(f"{angel} is an angel on Supernatural.")
+print(f"{human} is a human on Supernatural.")
+print(f"{demon} is a demon on Supernatural.")
+print(f"{archangel} is an archangel on Supernatural.")
